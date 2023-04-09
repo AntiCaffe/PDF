168a79213d723d5c1dc4f5130af2a57b43426792
ysmac <ivneld@naver.com>
2023-04-09 12:07:42 +0900

    Test folder error Edit

--- src/test/java/project/capstone/CapstoneApplicationTests.java
+++ src/test/java/project/capstone/CapstoneApplicationTests.java
@@ -3,7 +3,7 @@ package project.capstone;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class CapstoneApplicationTests {
 
 	@Test
