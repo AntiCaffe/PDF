604f305c1250fe1d7bed7eacc35391506f67ea39
ysmac <ivneld@naver.com>
2023-04-15 01:59:22 +0900

    login controller

--- src/main/java/project/capstone/repository/MemberRepositoryCustom.java
+++
@@ -1,9 +0,0 @@
-package project.capstone.repository;
-
-import project.capstone.controller.dto.MemberResponseDto;
-
-public interface MemberRepositoryCustom {
-
-    public MemberResponseDto findByNicknameAndPassword(String nickname, String password);
-
-}
