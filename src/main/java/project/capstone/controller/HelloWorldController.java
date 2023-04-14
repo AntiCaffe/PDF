8786504fe5ce0e8e51852ef53895cb2091740c07
ysmac <ivneld@naver.com>
2023-04-14 11:55:51 +0900

    test

--- src/main/java/project/capstone/controller/HelloWorldController.java
+++ src/main/java/project/capstone/controller/HelloWorldController.java
@@ -1,13 +1,36 @@
 package project.capstone.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import project.capstone.controller.dto.MemberResponseDto;
+import project.capstone.entity.Member;
+import project.capstone.repository.MemberRepository;
 
+import java.util.List;
+
+@CrossOrigin(origins = "*")
 @RestController
 public class HelloWorldController {
+
+    @Autowired
+    MemberRepository memberRepository;
 
     @GetMapping("/authentication/sing-in")
     public String test() {
         return "Hello, world!";
+    }
+
+    @PostMapping("/authentication/sign-in")
+    public MemberResponseDto login(@RequestBody final MemberResponseDto param) {
+        String nickname = param.getNickname();
+        String password = param.getPassword();
+
+        MemberResponseDto member = memberRepository.findByNicknameAndPassword(nickname, password);
+
+        return member;
+    }
+    @GetMapping("/test/members")
+    public List<Member> memberList() {
+        return memberRepository.findAll();
     }
 }
--- src/main/java/project/capstone/repository/MemberRepository.java
+++ src/main/java/project/capstone/repository/MemberRepository.java
@@ -1,7 +1,10 @@
 package project.capstone.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import project.capstone.controller.dto.MemberResponseDto;
 import project.capstone.entity.Member;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    MemberResponseDto findByNicknameAndPassword(String nickname, String password);
 }
