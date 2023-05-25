b30accbe5e57708fc8cfb92b158a9ef730035472
JSPARK <flash246@naver.com>
2023-05-26 06:22:21 +0900

    feat(core): 이미지 리스트 구현
    이미지 리스트를 서버에서 받아와 스크롤바가 있는 리스트로 구현하고, 파일 이름을 누르면 이미지가 뜨도록 했습니다.

--- src/main/frontend/src/components/normalGallery/index.js
+++ src/main/frontend/src/components/normalGallery/index.js
@@ -1,8 +1,12 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { Box, List, ListItem, ListItemText } from "@mui/material";
+import "./index.css";
+import no_img from "src/assets/images/no_img.jpg";
 
 const Dashboard = () => {
   const [names, setNames] = useState([]);
+  const [selectedImage, setSelectedImage] = useState("no_img"); // 선택한 이미지의 URL
 
   useEffect(() => {
     fetchData();
@@ -19,14 +23,55 @@ const Dashboard = () => {
     }
   };
 
+  const handleImageClick = async (name) => {
+    try {
+      const response = await axios.get("http://localhost:8080/dashboard/items");
+      const data = response.data;
+      const imageUrl = data.find((item) => item.name === name)?.imageUrl;
+      setSelectedImage(imageUrl);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div>
-      <h1>Names</h1>
-      <ul>
-        {names.map((name, index) => (
-          <li key={index}>{name}</li>
-        ))}
-      </ul>
+      <div className="container">
+        <h1>정상품목</h1>
+        <div className="content">
+          <div className="normal-list scrollableList">
+            <List
+              sx={{
+                backgroundColor: "#4452a0",
+                borderRadius: "5px 5px 0 0",
+              }}
+            >
+              <div
+                style={{
+                  justifyContent: "center",
+                }}
+              >
+                {names.map((name, index) => (
+                  <ListItem key={index} onClick={() => handleImageClick(name)}>
+                    <ListItemText primary={name} />
+                  </ListItem>
+                ))}
+              </div>
+            </List>
+          </div>
+          <div className="imageContainer">
+            {selectedImage !== "no_img" ? (
+              <img
+                src={selectedImage}
+                alt="Selected Image"
+                className="normal-img"
+              />
+            ) : (
+              <img src={no_img} alt="No Image" className="normal-img" />
+            )}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
